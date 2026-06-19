cd3fd6f68d0a37a2be65bd66a63d9a885d662434
Jesse <3191955858@qq.com>
2026-06-19 20:42:00 +0800

    Linux 内核补丁邮件推送器接口。

---
+++ src/main/java/com/jesse/linux_kernel_email_list_analyzer/components/KernelEmailPusher.java
@@ -0,0 +1,11 @@
+package com.jesse.linux_kernel_email_list_analyzer.components;
+
+/** Linux 内核补丁邮件推送器接口。*/
+public interface KernelEmailPusher
+{
+    /**
+     * 手动的将邮箱中的未读内核补丁邮件推送到消息队列，
+     * 返回推送的邮件数量。
+     */
+    void push();
+}
