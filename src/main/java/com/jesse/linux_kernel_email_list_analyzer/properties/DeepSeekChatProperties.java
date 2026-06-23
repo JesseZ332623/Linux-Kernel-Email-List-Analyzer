005dd0af887109fd2aa49643d3a487d3163f94e3
浩进 朱 <3191955858@qq.com>
2026-06-23 11:57:48 +0800

    DeepSeek 模型对话属性配置类。

---
+++ src/main/java/com/jesse/linux_kernel_email_list_analyzer/properties/DeepSeekChatProperties.java
@@ -0,0 +1,40 @@
+package com.jesse.linux_kernel_email_list_analyzer.properties;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/** DeepSeek 模型对话属性配置类。*/
+@Data
+@ToString
+@EqualsAndHashCode
+@Component
+@ConfigurationProperties(prefix = "app.deepseek-chat")
+public class DeepSeekChatProperties
+{
+    /** AI 模型对话端点 */
+    private String modelEndpointUrl;
+
+    /** 模型名称 */
+    private String modelName;
+
+    /** 端点访问身份凭证名 */
+    private String authorizationName;
+
+    /** 是否开启推理模式 */
+    private String thinking;
+
+    /** 模型的推理强度 */
+    private String reasoningEffort;
+
+    /** 是否流式输出 */
+    private boolean stream;
+
+    /** 系统提示词文件所在的类路径 */
+    private String sysPromptsClasspath;
+
+    /** 用户提示词文件所在的类路径 */
+    private String usrPromptsClasspath;
+}
