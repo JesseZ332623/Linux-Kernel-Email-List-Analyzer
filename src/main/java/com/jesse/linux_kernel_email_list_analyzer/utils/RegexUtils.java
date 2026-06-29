991de6ba78cc8180cb60299e85a689b2d59e3ea1
浩进 朱 <3191955858@qq.com>
2026-06-29 09:54:52 +0800

    正则表达式工具类。

---
+++ src/main/java/com/jesse/linux_kernel_email_list_analyzer/utils/RegexUtils.java
@@ -0,0 +1,15 @@
+package com.jesse.linux_kernel_email_list_analyzer.utils;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.util.regex.Pattern;
+
+/** 正则表达式工具类。*/
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+final public class RegexUtils
+{
+    /** Windows 非法文件名字符正则表达式匹配。*/
+    public static Pattern ILLEGAL_CHARACTOR_PATTERN
+        = Pattern.compile("[<>:\"/\\\\|?*=()]");
+}
