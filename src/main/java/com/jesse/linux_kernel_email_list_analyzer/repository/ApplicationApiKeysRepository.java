4d36148f7c7a8118e481f375be2ce19757faffb3
Jesse <3191955858@qq.com>
2026-07-12 00:21:06 +0800

    第三方应用访问 API Keys 表仓库类。

--- src/main/java/com/jesse/linux_kernel_email_list_analyzer/repository/ApplicationApiKeysRepository.java
+++ src/main/java/com/jesse/linux_kernel_email_list_analyzer/repository/ApplicationApiKeysRepository.java
@@ -1,23 +1,24 @@
 package com.jesse.linux_kernel_email_list_analyzer.repository;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.jesse.linux_kernel_email_list_analyzer.entity.ApplicationApiKeys;
-import org.springframework.data.jdbc.repository.query.Query;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 /** 第三方应用访问 API Keys 表仓库类。*/
-@Repository
+@Mapper
 public interface ApplicationApiKeysRepository
-    extends CrudRepository<ApplicationApiKeys, Long>
+    extends BaseMapper<ApplicationApiKeys>
 {
     /** 通过第三方 APP 名查询对应的 API Key。*/
-    @Query("""
+    @Select("""
         SELECT
             api_key
         FROM
             application_api_keys
         WHERE
-            application_name = :appName
+            application_name = #{appName}
     """)
-    String findByAppName(String appName);
+    String findByAppName(@Param("appName") String appName);
 }
