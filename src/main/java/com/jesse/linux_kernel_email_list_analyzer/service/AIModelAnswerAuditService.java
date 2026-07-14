6b0bacdaa6e6184ce485ce2c95eeda61d1fc6bb7
浩进 朱 <3191955858@qq.com>
2026-07-14 17:10:55 +0800

    AI 模型 LKML 分析任务响应审计表服务类接口。

--- src/main/java/com/jesse/linux_kernel_email_list_analyzer/service/AIModelAnswerAuditService.java
+++ src/main/java/com/jesse/linux_kernel_email_list_analyzer/service/AIModelAnswerAuditService.java
@@ -1,10 +1,11 @@
 package com.jesse.linux_kernel_email_list_analyzer.service;
 
+import com.jesse.linux_kernel_email_list_analyzer.pojo.PlainTextEmail;
 import com.jesse.linux_kernel_email_list_analyzer.response.AIModelAnswerResponse;
 
 /** AI 模型 LKML 分析任务响应审计表服务类接口。*/
 public interface AIModelAnswerAuditService
 {
     /** 执行完一封内核邮件的分析后，存储本次分析的审计信息。*/
-    void save(AIModelAnswerResponse response);
+    void save(PlainTextEmail email, AIModelAnswerResponse response);
 }
