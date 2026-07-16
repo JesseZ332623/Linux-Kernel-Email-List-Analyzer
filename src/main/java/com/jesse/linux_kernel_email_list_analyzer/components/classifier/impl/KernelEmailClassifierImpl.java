4508a3ad470d3a44c5ff7aab399195f26d73f728
Jesse <101250851+JesseZ332623@users.noreply.github.com>
2026-07-16 11:14:31 +0800

    紧急修复：内核补丁邮件标题的降级匹配 (#24)

--- src/main/java/com/jesse/linux_kernel_email_list_analyzer/components/classifier/impl/KernelEmailClassifierImpl.java
+++ src/main/java/com/jesse/linux_kernel_email_list_analyzer/components/classifier/impl/KernelEmailClassifierImpl.java
@@ -11,13 +11,12 @@ import org.springframework.stereotype.Component;
 import java.io.UnsupportedEncodingException;
 import java.nio.file.Path;
 import java.util.Locale;
+import java.util.Map;
 import java.util.Objects;
-import java.util.Set;
 import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import static com.jesse.linux_kernel_email_list_analyzer.components.classifier.KernelEmailEventType.*;
-import static java.lang.String.format;
+import static com.jesse.linux_kernel_email_list_analyzer.utils.KernelEmailClassifierUtils.*;
 
 /** 内核邮件分类器实现类。*/
 @Slf4j
@@ -25,61 +24,6 @@ import static java.lang.String.format;
 @RequiredArgsConstructor
 public class KernelEmailClassifierImpl implements KernelEmailClassifier
 {
-    /** 补丁回复邮件的标题前缀。*/
-    private static final
-    Set<String> REPLY_PREFIX = Set.of("RE", "Re", "re");
-
-    /**
-     * 对于一些通过别的邮箱服务转发的内核补丁邮件，比如：
-     *
-     * <pre>
-     * {@literal Wentao Liang <vulab@iscas.ac.cn> 通过“vger.kernel.org” }
-     * </pre>
-     *
-     * 如果拿原始的 from 直接分类的话会造成混乱，
-     * 所以本正则表达式可以完整的抓取作者 + {@literal <作者邮箱>} 的信息。
-     *
-     * <h4>2026.06.30 修复</h4>
-     * 至于 LKML 中某些不守规矩的邮件，
-     * 比如 from 就是简单的 phucduc.bui@gmail.com 纯邮箱地址，
-     * 正则表达式需要为这种莽撞的情况兜底，如下所示：
-     *
-     * <pre>
-     * ^                # 从行首开始
-     * (?:              # 非捕获组，整体是 "要么匹配这个，要么匹配那个"
-     *      .*?         #   分支 A：非贪婪匹配显示名
-     *      <           #         尖括号开始
-     *        (         #         捕获组 1（尖括号里的邮箱）
-     *          [^>]+   #           尖括号内的内容
-     *        )         #         捕获组 1 结束
-     *      >           #         尖括号结束
-     *   |              #   或
-     *      (           #   分支 B：捕获组 2（纯邮箱兜底）
-     *        .+@.+     #         至少一个字符 + @ + 至少一个字符
-     *      )           #   捕获组 2 结束
-     * )                # 非捕获组结束
-     * </pre>
-     */
-    private static final
-    Pattern FROM_PATTERN = Pattern.compile("^(?:.*?<([^>]+)>|(.+@.+))");
-
-    /**
-     * 提取内核补丁邮件标题开头中 [] 内的完整字符串。
-     *
-     * <pre><code>
-     * String subject
-     *     = "[PATCH v3 net] ipv6: fib6: fix NULL deref in fib6_walk_continue() on multi-batch dump";
-     *
-     * Matcher matcher = TAG_BLOCK_PATTERN.matcher(subject);
-     *
-     * matcher.find();
-     *
-     * matcher.group(1); // PATCH v3 net
-     * </code></pre>
-     */
-    private static final
-    Pattern TAG_BLOCK_PATTERN = Pattern.compile("^\\[([^]]+)]");
-
     /**
      * 对于非英语国家的内核开发者提交补丁邮件，
      * 邮箱标题等元数据信息需要解码 RFC 2047 解码。
@@ -112,12 +56,26 @@ public class KernelEmailClassifierImpl implements KernelEmailClassifier
         final Matcher matcher
             = TAG_BLOCK_PATTERN.matcher(subject);
 
+        /*
+         * 如果这封邮件标题不是完全符合标准的，
+         * 则把他代入标题降级分类正则表进一步匹配，
+         * 如果还是找不到则返回 UNKNOW，确保不丢件。
+         */
         if (!matcher.find())
         {
-            throw new
-            IllegalArgumentException(
-                format("Subject: %s is not a standard LKML.", subject)
+            log.warn(
+                "Subject: {} is not a stanard LKML, " +
+                "try matching with fallback patterns.",
+                subject
             );
+
+            return
+            FALLBACK_PATTERNS.entrySet().stream()
+                .filter((entry) ->
+                    entry.getKey().matcher(subject).find())
+                .map(Map.Entry::getValue)
+                .findFirst()
+                .orElse(UNKNOWN);
         }
 
         final String event
@@ -135,7 +93,7 @@ public class KernelEmailClassifierImpl implements KernelEmailClassifier
     }
 
     /** 按 FROM_PATTERN 正确提取发件人信息（作者 + 作者邮箱）。*/
-    private String extractFrom(String from)
+    private static String extractFrom(String from)
     {
         if (Objects.isNull(from)) {
             throw new IllegalArgumentException("Argument <from> must not be null.");
