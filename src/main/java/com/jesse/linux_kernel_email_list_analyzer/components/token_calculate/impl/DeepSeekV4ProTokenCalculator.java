cd244bbac424c31d381abf08c07a6c5ee69a6b68
浩进 朱 <3191955858@qq.com>
2026-07-17 10:25:16 +0800

    DeepSeek-V4-Pro 模型 Token 资费计算器。
    - 新增峰期消耗双倍计算资费的逻辑

--- src/main/java/com/jesse/linux_kernel_email_list_analyzer/components/token_calculate/impl/DeepSeekV4ProTokenCalculator.java
+++ src/main/java/com/jesse/linux_kernel_email_list_analyzer/components/token_calculate/impl/DeepSeekV4ProTokenCalculator.java
@@ -52,10 +52,14 @@ public class DeepSeekV4ProTokenCalculator implements ModelTokenCalculator
             = BigDecimal.valueOf(modelAnswerUsage.getCompletionTokens())
                         .divide(ONE_MILLION, PRCESION, RoundingMode.HALF_UP);
 
+        final BigDecimal standardPrice
+            = promptCacheHitTokens.multiply(PROMPT_CACHE_HIT_PRICE)
+                .add(promptCacheMissTokens.multiply(PROMPT_CACHE_MISS_PRICE))
+                .add(completionTokens.multiply(COMPLETION_PRICE));
+
         return
-        promptCacheHitTokens.multiply(PROMPT_CACHE_HIT_PRICE)
-            .add(promptCacheMissTokens.multiply(PROMPT_CACHE_MISS_PRICE))
-            .add(completionTokens.multiply(COMPLETION_PRICE))
-            .stripTrailingZeros();
+        (modelAnswerUsage.getIsPeak())
+            ? standardPrice.multiply(BigDecimal.TWO).stripTrailingZeros()
+            : standardPrice.stripTrailingZeros();
     }
 }
