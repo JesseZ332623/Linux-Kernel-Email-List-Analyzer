adcc234ee8bf2cd71ad0d351a9d1c546f6378e54
浩进 朱 <3191955858@qq.com>
2026-07-23 11:18:16 +0800

    Linux 内核补丁邮件推送器接口。
    - 移动到 com.jesse.linux_kernel_email_list_analyzer.components.kernel_email_pusher 包下

--- src/main/java/com/jesse/linux_kernel_email_list_analyzer/components/kernel_email_pusher/KernelEmailPusher.java
+++ src/main/java/com/jesse/linux_kernel_email_list_analyzer/components/kernel_email_pusher/KernelEmailPusher.java
@@ -1,4 +1,4 @@
-package com.jesse.linux_kernel_email_list_analyzer.components;
+package com.jesse.linux_kernel_email_list_analyzer.components.kernel_email_pusher;
 
 /** Linux 内核补丁邮件推送器接口。*/
 public interface KernelEmailPusher
