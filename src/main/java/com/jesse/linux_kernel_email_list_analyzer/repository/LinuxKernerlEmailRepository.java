3535d1ef2f226d30c86ac7f0727b406f0c8735a7
浩进 朱 <3191955858@qq.com>
2026-07-23 11:33:15 +0800

    内核邮件数据表仓储类。
    - 修改 updateAnalyzeStatusByXXX() 方法为 updateStatusByXXX()

--- src/main/java/com/jesse/linux_kernel_email_list_analyzer/repository/LinuxKernerlEmailRepository.java
+++ src/main/java/com/jesse/linux_kernel_email_list_analyzer/repository/LinuxKernerlEmailRepository.java
@@ -1,7 +1,7 @@
 package com.jesse.linux_kernel_email_list_analyzer.repository;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.jesse.linux_kernel_email_list_analyzer.constant.KernelEmailAnalyzeStatus;
+import com.jesse.linux_kernel_email_list_analyzer.components.state_machine.KernelEmailStatus;
 import com.jesse.linux_kernel_email_list_analyzer.entity.LinuxKernerlEmailEntiy;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Update;
@@ -11,27 +11,27 @@ import org.apache.ibatis.annotations.Update;
 public interface LinuxKernerlEmailRepository
     extends BaseMapper<LinuxKernerlEmailEntiy>
 {
-    /** 修改指定 taskId 的内核邮件的分析任务执行状态。*/
+    /** 修改指定 taskId 的内核邮件的状态。*/
     @Update("""
         UPDATE
             linux_kernal_email
         SET
-            analyze_status = #{status}
+            status = #{status}
         WHERE
             task_id = #{taskId}
     """)
-    int updateAnalyzeStatusByTaskId(String taskId, KernelEmailAnalyzeStatus status);
+    int updateStatusByTaskId(String taskId, KernelEmailStatus status);
 
-    /** 修改指定 id 的内核邮件的分析任务执行状态。*/
+    /** 修改指定 id 的内核邮件的状态。*/
     @Update("""
         UPDATE
             linux_kernal_email
         SET
-            analyze_status = #{status}
+            status = #{status}
         WHERE
             id = #{id}
     """)
-    int updateAnalyzeStatusById(Long id, KernelEmailAnalyzeStatus status);
+    int updateStatusById(Long id, KernelEmailStatus status);
 
     /** 将指定 id 的邮件与指定的分析任务关联。*/
     @Update("""
