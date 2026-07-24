84947cf685a582285c952a6fd57d54db72bdce5d
浩进 朱 <3191955858@qq.com>
2026-07-24 16:41:09 +0800

    AI 模型 LKML 分析任务响应审计实体转换器。

--- analyzer/src/main/java/com/jesse/analyzer/converter/AIModelAnswerAuditConverter.java
+++ analyzer/src/main/java/com/jesse/analyzer/converter/AIModelAnswerAuditConverter.java
@@ -1,14 +1,14 @@
-package com.jesse.linux_kernel_email_list_analyzer.converter;
+package com.jesse.analyzer.converter;
 
-import com.jesse.linux_kernel_email_list_analyzer.entity.AIModelAnswerAuditEntity;
-import com.jesse.linux_kernel_email_list_analyzer.response.AIModelAnswerResponse;
+import com.jesse.analyzer.entity.AIModelAnswerAuditEntity;
+import com.jesse.analyzer.response.AIModelAnswerResponse;
 import org.springframework.stereotype.Component;
 
 /** AI 模型 LKML 分析任务响应审计实体转换器。*/
 @Component
 public class AIModelAnswerAuditConverter
 {
-    public  AIModelAnswerAuditEntity
+    public AIModelAnswerAuditEntity
     convert(Long nextId, AIModelAnswerResponse response)
     {
         final AIModelAnswerAuditEntity audit = new AIModelAnswerAuditEntity();
