5897d51f5131eb997a7bc2c3776907ad33f29062
浩进 朱 <3191955858@qq.com>
2026-07-24 16:37:06 +0800

    AI 模型回答内容表仓储类。

--- analyzer/src/main/java/com/jesse/analyzer/repository/AIModelAnswerContentRepository.java
+++ analyzer/src/main/java/com/jesse/analyzer/repository/AIModelAnswerContentRepository.java
@@ -1,7 +1,7 @@
-package com.jesse.linux_kernel_email_list_analyzer.repository;
+package com.jesse.analyzer.repository;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.jesse.linux_kernel_email_list_analyzer.entity.AIModelAnswerContentEntity;
+import com.jesse.analyzer.entity.AIModelAnswerContentEntity;
 import org.apache.ibatis.annotations.Mapper;
 
 /** AI 模型回答内容表仓储类。*/
