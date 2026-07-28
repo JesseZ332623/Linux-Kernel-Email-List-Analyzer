059d54dd9106df6d0a5606d4029128ac9a19c877
浩进 朱 <3191955858@qq.com>
2026-07-28 10:50:46 +0800

    全局 ID 消费机实现。
    - 新增 nextIds() 方法获取下一批 ID

--- core/src/main/java/com/jesse/core/components/global_id/impl/GlobalIdConsumerImpl.java
+++ core/src/main/java/com/jesse/core/components/global_id/impl/GlobalIdConsumerImpl.java
@@ -1,6 +1,7 @@
 package com.jesse.core.components.global_id.impl;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jesse.core.components.global_id.GlobalIdConsumer;
 import com.jesse.core.properties.IdConsumerProperties;
@@ -8,6 +9,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /** 全局 ID 消费机实现。*/
 @Slf4j
@@ -41,10 +44,41 @@ public class GlobalIdConsumerImpl implements GlobalIdConsumer
                 .get("data")
                 .asLong();
         }
+        catch (JsonProcessingException exception) {
+            log.error("Unexpected response {}", responseJSON, exception);
+            return -1;
+        }
+    }
+
+    /** 获取下一批 ID */
+    @Override
+    public List<Long> nextIds(int batchSize)
+    {
+        if (batchSize <= 0) {
+            throw new IllegalArgumentException("Param batchSize must large then 0");
+        }
+
+        final String nextIdsEndPoint
+            = this.properties.getDirectoryUrls().getNextBatch()
+                + "?size=" + batchSize;
+
+        final String responseJSON
+            = this.restTemplate.getForObject(nextIdsEndPoint, String.class);
+
+        try
+        {
+            return
+            this.objectMapper.readValue(
+                this.objectMapper.readTree(responseJSON)
+                    .get("data")
+                    .toString(),
+                new TypeReference<>() {}
+            );
+        }
         catch (JsonProcessingException exception)
         {
             log.error("Unexpected response {}", responseJSON, exception);
-            return -1;
+            return List.of();
         }
     }
 }
