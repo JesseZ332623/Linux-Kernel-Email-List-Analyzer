48bfb28b464a924b175b769dc088f2b4f957b65a
浩进 朱 <3191955858@qq.com>
2026-08-03 17:23:20 +0800

    内核邮件 -> AI 模型 分析器接口。

--- analyzer/src/main/java/com/jesse/analyzer/components/kernel_email_analyzer/KernelEmailAIModelAnalyzer.java
+++ analyzer/src/main/java/com/jesse/analyzer/components/kernel_email_analyzer/KernelEmailAIModelAnalyzer.java
@@ -1,10 +1,10 @@
 package com.jesse.analyzer.components.kernel_email_analyzer;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.jesse.analyzer.response.AIModelAnswerResponse;
+import com.jesse.core.response.AIModelAnswerResponse;
 import com.jesse.core.pojo.PlainTextEmail;
 
-/** 内核邮件 -> AI 模型 分析器接口类。*/
+/** 内核邮件 -> AI 模型 分析器接口。*/
 public interface KernelEmailAIModelAnalyzer
 {
     /** 将内核邮件数据提交给 AI 模型分析，返回分析结果响应实例。*/
