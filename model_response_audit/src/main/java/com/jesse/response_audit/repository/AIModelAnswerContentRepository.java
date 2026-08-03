673fdab168c2151b5baf09ea6ea3106c52f358a1
浩进 朱 <3191955858@qq.com>
2026-08-03 17:07:02 +0800

    AI 模型回答内容表仓储类。
    - 从 analyzer 模块移动到 core 模块

--- model_response_audit/src/main/java/com/jesse/response_audit/repository/AIModelAnswerContentRepository.java
+++ model_response_audit/src/main/java/com/jesse/response_audit/repository/AIModelAnswerContentRepository.java
@@ -1,7 +1,7 @@
-package com.jesse.analyzer.repository;
+package com.jesse.response_audit.repository;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.jesse.analyzer.entity.AIModelAnswerContentEntity;
+import com.jesse.core.entity.AIModelAnswerContentEntity;
 import org.apache.ibatis.annotations.Mapper;
 
 /** AI 模型回答内容表仓储类。*/
