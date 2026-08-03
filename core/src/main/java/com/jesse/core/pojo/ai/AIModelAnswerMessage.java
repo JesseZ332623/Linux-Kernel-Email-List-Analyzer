97338e5ec75e70b59b1890f5a81970d8373cb1ff
浩进 朱 <3191955858@qq.com>
2026-08-03 16:54:33 +0800

    AI 模型回复消息类。
    - 从 analyzer 模块移动至 core 模块

--- core/src/main/java/com/jesse/core/pojo/ai/AIModelAnswerMessage.java
+++ core/src/main/java/com/jesse/core/pojo/ai/AIModelAnswerMessage.java
@@ -1,10 +1,7 @@
-package com.jesse.analyzer.pojo.ai;
+package com.jesse.core.pojo.ai;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 /**
  * AI 模型回复消息类，JSON 示例如下：
@@ -21,6 +18,7 @@ import lombok.ToString;
 @ToString
 @EqualsAndHashCode
 @NoArgsConstructor
+@AllArgsConstructor
 public class AIModelAnswerMessage
 {
     /** 固定为 "assistant"，表示这是AI的回复 */
