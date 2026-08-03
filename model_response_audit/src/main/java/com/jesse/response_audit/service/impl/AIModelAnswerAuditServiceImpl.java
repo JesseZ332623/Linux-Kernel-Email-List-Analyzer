1167dfc5231e666adfedc70ff34824b2a72f49fd
浩进 朱 <3191955858@qq.com>
2026-08-03 17:09:11 +0800

    AI 模型 LKML 分析任务响应审计表服务类实现。
    - 从 analyzer 模块移动到 core 模块

--- model_response_audit/src/main/java/com/jesse/response_audit/service/impl/AIModelAnswerAuditServiceImpl.java
+++ model_response_audit/src/main/java/com/jesse/response_audit/service/impl/AIModelAnswerAuditServiceImpl.java
@@ -1,19 +1,18 @@
-package com.jesse.analyzer.service.impl;
+package com.jesse.response_audit.service.impl;
 
-import com.jesse.analyzer.converter.AIModelAnswerAuditConverter;
-import com.jesse.analyzer.converter.AIModelAnswerContentConverter;
-import com.jesse.analyzer.converter.AIModelAnswerUsageConverter;
-import com.jesse.analyzer.entity.AIModelAnswerAuditEntity;
-import com.jesse.analyzer.entity.AIModelAnswerContentEntity;
-import com.jesse.analyzer.entity.AIModelAnswerUsageEntity;
-import com.jesse.analyzer.repository.AIModelAnswerAuditRepository;
-import com.jesse.analyzer.repository.AIModelAnswerContentRepository;
-import com.jesse.analyzer.repository.AIModelAnswerUsageRepository;
-import com.jesse.analyzer.response.AIModelAnswerResponse;
-import com.jesse.analyzer.service.AIModelAnswerAuditService;
-import com.jesse.analyzer.service.LinuxKernerlEmailService;
+import com.jesse.core.entity.AIModelAnswerAuditEntity;
+import com.jesse.core.entity.AIModelAnswerContentEntity;
+import com.jesse.core.entity.AIModelAnswerUsageEntity;
+
+import com.jesse.core.response.AIModelAnswerResponse;
 import com.jesse.core.components.global_id.GlobalIdConsumer;
-import com.jesse.core.pojo.PlainTextEmail;
+import com.jesse.response_audit.converter.AIModelAnswerAuditConverter;
+import com.jesse.response_audit.converter.AIModelAnswerContentConverter;
+import com.jesse.response_audit.converter.AIModelAnswerUsageConverter;
+import com.jesse.response_audit.repository.AIModelAnswerAuditRepository;
+import com.jesse.response_audit.repository.AIModelAnswerContentRepository;
+import com.jesse.response_audit.repository.AIModelAnswerUsageRepository;
+import com.jesse.response_audit.service.AIModelAnswerAuditService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -37,10 +36,6 @@ public class AIModelAnswerAuditServiceImpl implements AIModelAnswerAuditService
     private final
     AIModelAnswerUsageRepository aiModelAnswerUsageRepository;
 
-    /** 内核邮件数据表服务接口。*/
-    private final
-    LinuxKernerlEmailService linuxKernerlEmailService;
-
     /** AI 模型 LKML 分析任务响应审计实体转换器。*/
     private final
     AIModelAnswerAuditConverter answerAuditConverter;
@@ -56,10 +51,10 @@ public class AIModelAnswerAuditServiceImpl implements AIModelAnswerAuditService
     /** 全局 ID 消费机接口。*/
     private final GlobalIdConsumer idConsumer;
 
-    /** 执行完一封内核邮件的分析后，存储本次分析的审计信息。*/
+    /** 在向大模型发起一次调用后，存储本次调用的审计信息。*/
     @Override
     @Transactional(rollbackFor = Exception.class)
-    public void save(PlainTextEmail email, AIModelAnswerResponse response)
+    public void save(AIModelAnswerResponse response)
     {
         final AIModelAnswerAuditEntity audit
             = this.answerAuditConverter
