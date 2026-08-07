794015de73e51d528fb686d08bd65629d6a49c5f
浩进 朱 <3191955858@qq.com>
2026-08-07 13:40:46 +0800

    Mybatis-Plus 配置类。
    - 新添加分页拦截器 PaginationInnerInterceptor

--- core/src/main/java/com/jesse/core/config/MyBatisPlusConfig.java
+++ core/src/main/java/com/jesse/core/config/MyBatisPlusConfig.java
@@ -1,7 +1,9 @@
 package com.jesse.core.config;
 
+import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,6 +22,9 @@ public class MyBatisPlusConfig
 
         // 添加数据库乐观锁拦截器
         interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+
+        // 添加分页插件，并指定数据库类型为 MySQL
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
 
         return interceptor;
     }
