db39e14ea89518eec570e2307f848d4b3bd7d896
浩进 朱 <3191955858@qq.com>
2026-08-14 16:34:55 +0800

    内核补丁邮件分析报告持久化器接口。
    - 注释补全

--- analyzer/src/main/java/com/jesse/analyzer/components/report_persistence/LKMLAnalyzeReportWriter.java
+++ analyzer/src/main/java/com/jesse/analyzer/components/report_persistence/LKMLAnalyzeReportWriter.java
@@ -7,5 +7,12 @@ import java.io.IOException;
 /** 内核补丁邮件分析报告持久化器接口。*/
 public interface LKMLAnalyzeReportWriter
 {
+    /**
+     * 写入分析报告到指定位置。
+     *
+     * @param kernelEmailId  邮件在数据库中的 ID
+     * @param plainTextEmail 邮件数据实例
+     * @param htmlText       thymeleaf 生成的邮件分析报告 HTML 文本
+     */
     void write(Long kernelEmailId, PlainTextEmail plainTextEmail, String htmlText) throws IOException;
 }
