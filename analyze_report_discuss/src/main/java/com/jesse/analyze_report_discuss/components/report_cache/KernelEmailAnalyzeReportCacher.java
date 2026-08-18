9c7b46fde71f30a1c65f2f3f55231cf8e6188003
浩进 朱 <3191955858@qq.com>
2026-08-18 14:39:00 +0800

    拼写错误变更（Kenel->Kernel）

--- analyze_report_discuss/src/main/java/com/jesse/analyze_report_discuss/components/report_cache/KernelEmailAnalyzeReportCacher.java
+++ analyze_report_discuss/src/main/java/com/jesse/analyze_report_discuss/components/report_cache/KernelEmailAnalyzeReportCacher.java
@@ -1,13 +1,13 @@
 package com.jesse.analyze_report_discuss.components.report_cache;
 
-import com.jesse.analyze_report_discuss.dto.KenelEmailAnalyzeReport;
+import com.jesse.analyze_report_discuss.dto.KernelEmailAnalyzeReport;
 
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
 /** 内核邮件分析报告缓存器接口。*/
-public interface KenelEmailAnalyzeReportCacher
+public interface KernelEmailAnalyzeReportCacher
 {
     /** 从缓存中获取分析报告数据。*/
-    Optional<KenelEmailAnalyzeReport> getOrLoad(String taskId);
+    Optional<KernelEmailAnalyzeReport> getOrLoad(String taskId);
 }
