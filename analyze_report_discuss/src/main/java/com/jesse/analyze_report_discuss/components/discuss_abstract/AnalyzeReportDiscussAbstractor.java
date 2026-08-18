c83d28321fea24dd8f9643387043f7bc133e36c3
浩进 朱 <3191955858@qq.com>
2026-08-18 14:39:01 +0800

    拼写错误变更（Reqest->Request）

--- analyze_report_discuss/src/main/java/com/jesse/analyze_report_discuss/components/discuss_abstract/AnalyzeReportDiscussAbstractor.java
+++ analyze_report_discuss/src/main/java/com/jesse/analyze_report_discuss/components/discuss_abstract/AnalyzeReportDiscussAbstractor.java
@@ -1,6 +1,6 @@
 package com.jesse.analyze_report_discuss.components.discuss_abstract;
 
-import com.jesse.analyze_report_discuss.request.DiscussAbstractReqest;
+import com.jesse.analyze_report_discuss.request.DiscussAbstractRequest;
 
 /** 分析报告讨论上下文摘要器接口。*/
 public interface AnalyzeReportDiscussAbstractor
@@ -9,5 +9,5 @@ public interface AnalyzeReportDiscussAbstractor
      * 一轮问答结束后，
      * 将本轮问答的信息交给轻量级模型做摘要并缓存，作为下一次对话的上下文。
      */
-    void discussAbstract(DiscussAbstractReqest reqest);
+    void discussAbstract(DiscussAbstractRequest request);
 }
