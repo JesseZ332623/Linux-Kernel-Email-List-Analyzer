8fdc49644f1fa51e9304729cd4348373f0bac724
浩进 朱 <3191955858@qq.com>
2026-08-24 11:22:43 +0800

    RabbitMQ 回退消息处理器接口。
    - 新增 BUSINESS_DOMAIN_KEY 业务域消息头 key 常量
    
    - handler() 方法新增 throws Exception

--- core/src/main/java/com/jesse/core/components/rabbitmq_callback/ReturnedMessageHandler.java
+++ core/src/main/java/com/jesse/core/components/rabbitmq_callback/ReturnedMessageHandler.java
@@ -5,14 +5,17 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 
 /**
  * RabbitMQ 回退消息处理器接口。
- * 未来如果有多个模块需要共用同一个 {@link RabbitTemplate} 处理消息回退，
+ * 如果有多个模块需要共用同一个 {@link RabbitTemplate} 处理消息回退，
  * 则可以实现本接口来表达 “某个业务要怎么回退消息”。
  */
 public interface ReturnedMessageHandler
 {
+    /** 业务域消息头 key */
+    String BUSINESS_DOMAIN_KEY = "business-domain";
+
     /** 哪个业务域？ */
     String businessDomain();
 
     /** 处理一条回退的消息。*/
-    void handler(ReturnedMessage returned);
+    void handler(ReturnedMessage returned) throws Exception;
 }
