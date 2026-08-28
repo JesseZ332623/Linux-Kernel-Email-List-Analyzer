26b5e25298f4cef4257d95a495e244d9a1d85f73
Jesse <3191955858@qq.com>
2026-08-28 13:57:14 +0800

    对某个分析报告发起讨论的请求体。
    - 提供完整的 Swagger 文档注解

--- analyze_report_discuss/src/main/java/com/jesse/analyze_report_discuss/request/ReportDiscussRequest.java
+++ analyze_report_discuss/src/main/java/com/jesse/analyze_report_discuss/request/ReportDiscussRequest.java
@@ -1,8 +1,11 @@
 package com.jesse.analyze_report_discuss.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
 
 /** 对某个分析报告发起讨论的请求体。*/
 @Data
@@ -11,12 +14,27 @@ import lombok.NoArgsConstructor;
 public class ReportDiscussRequest
 {
     /** 内核邮件分析报告 ID */
+    @Schema(
+        description  = "内核邮件分析报告 ID",
+        example      = "2082002037282996224",
+        requiredMode = REQUIRED
+    )
     private String taskId;
 
     /** 会话 ID */
+    @Schema(
+        description  = "会话 ID",
+        example      = "70462e7e-7269-4f03-9f72-81b04ae5c0a4",
+        requiredMode = REQUIRED
+    )
     private String sessionId;
 
     /** 提出的问题 */
+    @Schema(
+        description  = "提出的问题",
+        example      = "在 Linux 内核维护中，使用条件编译为什么是草率的、业余的？",
+        requiredMode = REQUIRED
+    )
     private String question;
 
     public String toString()
