4fd83e666aa4b06c42bd044b0f05290e8b7ab0d7
Jesse <3191955858@qq.com>
2026-09-07 20:54:10 +0800

    讨论会话管理出现的错误抛本异常。
    - 修改 make() 工厂方法，新增 message 形参用于表达更详细的异常信息

--- analyze_report_discuss/src/main/java/com/jesse/analyze_report_discuss/exception/DiscussSessionException.java
+++ analyze_report_discuss/src/main/java/com/jesse/analyze_report_discuss/exception/DiscussSessionException.java
@@ -1,5 +1,7 @@
 package com.jesse.analyze_report_discuss.exception;
 
+import org.springframework.util.StringUtils;
+
 import static java.lang.String.format;
 
 /** 讨论会话管理出现的错误抛本异常。*/
@@ -9,11 +11,14 @@ public class DiscussSessionException extends RuntimeException
         super(message);
     }
 
-    public static DiscussSessionException make(String taskId)
+    public static DiscussSessionException make(String taskId, String message)
     {
         return new
         DiscussSessionException(
-            format("Create discuss session for %s failed.", taskId)
+            format(
+                "Create discuss session for %s failed. (%s)",
+                taskId, StringUtils.hasText(message) ? message : "Unknown"
+            )
         );
     }
 }
